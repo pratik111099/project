798c8145ad9ca7eb83bb4a672cd329d5448dd009
pratik111099 <pratik.deshmukh800@gmail.com>
2023-09-13 07:36:47 +0530

    searchnewfeatcher

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Good Morning</p>
+        <p>Good Day</p>
         <a
           className="App-link"
           href="https://reactjs.org"
